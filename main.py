34618f69c15100487f29e59440a4bec9679fb626
Rafal Sota <biuro@unico-group.pl>
2024-12-19 15:18:07 +0100

    Add loop for

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ products = {
     "piekarnia" : ["chleb", "bułki", "pączek"],
     "warzywniak" : ["marchew", "seler", "rukola"]
 }
+
+for shop, product in products.items():
+    produkty = [p.capitalize() for p in product] 
+    print(f"Idę do {shop.capitalize()} i kupuję tam {produkty}")
