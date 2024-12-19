94713b8078e80cda60f891b36700d42545259a34
Rafal Sota <biuro@unico-group.pl>
2024-12-19 15:20:44 +0100

    Capitalized - shops and products

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ products = {
     "piekarnia" : ["chleb", "bułki", "pączek"],
     "warzywniak" : ["marchew", "seler", "rukola"]
 }
+l = 0
 
+print("\nLista zakupów: ")
 for shop, product in products.items():
     produkty = [p.capitalize() for p in product] 
     print(f"Idę do {shop.capitalize()} i kupuję tam {produkty}")
+    l = l + len(product)
+print(f"W sumie kupuję {l} produktów\n")
