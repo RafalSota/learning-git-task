0eba5a2b4a51d52c2171dcdd2025dbd8fba07068
Rafal Sota <biuro@unico-group.pl>
2024-12-22 16:35:52 +0100

    wishes for mentor

--- main.py
+++ main.py
@@ -12,3 +12,5 @@ for shop, product in products.items():
 print(f"W sumie kupuję {l} produktów\n")
 
 print("Dodatkowa zmiana do nowego commit")
+
+print("\n\033[1mPatryku, Życzę Ci spokojnych i radosnych Świąt, pełnych inspiracji i odpoczynku. \nNiech Nowy Rok przyniesie Ci satysfakcję z osiągnięć, nowe wyzwania i wiele sukcesów w świecie kodu i AI. \nDziękuję za Twoje wsparcie.\n\033[0m")
